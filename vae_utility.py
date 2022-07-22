765b9933cd3fc636e093f31ced0c5a501f1097bb
Oliver-Tautz <53008918+Oliver-Tautz@users.noreply.github.com>
2022-07-22 13:22:14 +0200

    Update vae_utility.py
    use default font

--- vae_utility.py
+++ vae_utility.py
@@ -15,7 +15,8 @@ from vae_parameters import *
 from vae_nets import *
 
 THRESHOLD = 50
-font = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf", 10)
+#font = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf", 10)
+font = = ImageFont.load_default()
 titles = ["orig img\n+crit val", "crit val\ninjected", "crit=0\ninjected", "difference\nmask", f"thr-mask\nthr={THRESHOLD}", "thr-mask +\ncrf", "ground\ntruth"]
 
 # copied from critic-code, source and github link are listed in bachelor thesis
